a23796df9314f3991e67701dc50e63123e2f2f2c
Justin Ting <24041609@myrp.edu.sg>
2025-07-22 21:36:20 +0800

    connection

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-//harizwashere
-//usagibombommm
-//testing
+const express = require('express');
+const mysql = require('mysql2');
+const multer = require('multer');
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage});
+
+// Create MySQL connection
+const connection = mysql.createConnection({
+  host: 'dddgt5.h.filess.io',
+  user: 'C237StudyBuddy_collegedie',
+  password: '768f143d94d757f1499c22e82cd2786488a7d407',
+  database: 'C237StudyBuddy_collegedie'
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
