bb7e8bd7a08528a94bd2173125cec6835f41108a
Justin Ting <24041609@myrp.edu.sg>
2025-07-24 14:06:58 +0800

    change

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
+
+const session = require('express-session');
+const flash = require('connect-flash');
+
 const multer = require('multer');
 const path = require('path');
 
